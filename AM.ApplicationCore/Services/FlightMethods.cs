3b2ec041e6d35956ff014a179f2b14bb3b452815
SeifZnati <seifeddine.znati@esprit.tn>
2025-02-06 10:51:44 +0100

    TP 2

---
+++ AM.ApplicationCore/Services/FlightMethods.cs
@@ -0,0 +1,56 @@
+﻿using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Interfaces;
+
+namespace AM.ApplicationCore.Services
+{
+    public class FlightMethods : IFlightMethods
+    {
+        public List<Flight> Flights { get; set; } = new List<Flight> { };
+
+        //Avec For
+        //public IList<DateTime> GetFlightDates(string destination)
+        //{
+        //    IList<DateTime> dates = new List<DateTime> { };
+        //    for (int i = 0; i < Flights.Count; i++)
+        //    {
+        //        if (Flights[i].Destination.Equals(destination))
+        //        {
+        //            dates.Add(Flights[i].FlightDate);
+        //        }
+        //    }
+        //    return dates;
+        //}
+
+        //Avec ForEach
+        public IList<DateTime> GetFlightDates(string destination)
+        {
+            IList<DateTime> dates = new List<DateTime> { };
+            foreach (var flight in Flights)
+            {
+                {
+                    if (flight.Destination.Equals(destination))
+                    {
+                        dates.Add(flight.FlightDate);
+                    }
+                }
+            }
+            return dates;
+        }
+
+        public void GetFlights(string filterType, string filterValue)
+        {
+            switch (filterType)
+            {
+                case "Destination":
+                    foreach (var flight in Flights)
+                    {
+                        if (flight.Destination.Equals(filterValue))
+                        {
+                            System.Console.WriteLine(flight.ToString());
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
--- AM.UI.Console/Program.cs
+++ AM.UI.Console/Program.cs
@@ -1,44 +1,58 @@
 ﻿using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Services;
 
-//Plane plane1 = new Plane();
-//plane1.Capacity = 100;
-//plane1.ManufactureDate = new DateTime(2024, 05, 23);
-//plane1.PlaneId = 1;
-//plane1.PlaneType = PlaneType.Boeing;
-//Console.WriteLine(plane1.ToString());
-//Plane plane2 = new Plane(PlaneType.Airbus, 200, DateTime.Now);
-//Console.WriteLine(plane2.ToString());
+/****** Atelier 1 ******/
 
-Plane plane3 = new Plane
+////Plane plane1 = new Plane();
+////plane1.Capacity = 100;
+////plane1.ManufactureDate = new DateTime(2024, 05, 23);
+////plane1.PlaneId = 1;
+////plane1.PlaneType = PlaneType.Boeing;
+////Console.WriteLine(plane1.ToString());
+////Plane plane2 = new Plane(PlaneType.Airbus, 200, DateTime.Now);
+////Console.WriteLine(plane2.ToString());
+
+//Plane plane3 = new Plane
+//{
+//    Capacity = 300,
+//    ManufactureDate = DateTime.Now,
+//    PlaneId = 3,
+//    PlaneType = PlaneType.Airbus
+//};
+//Plane plane4 = new Plane { };
+//Console.WriteLine(plane3.ToString());
+//Console.WriteLine(plane4.ToString());
+
+//Passenger passenger = new Passenger
+//{
+//    FirstName = "Seif",
+//    LastName = "Znati",
+//    EmailAddress = "seifeddine.znati@esprit.tn"
+//};
+//Console.WriteLine(passenger.ToString());
+//Console.WriteLine(passenger.CheckProfile("Seif", "Znati"));
+//Console.WriteLine(passenger.CheckProfile("Ons", "Znati"));
+//Console.WriteLine(passenger.CheckProfile("Seif", "Znati","seifeddine.znati@esprit.tn"));
+//Console.WriteLine(passenger.CheckProfile("Seif", "Znati", "znatiseif@gmail.com"));
+//passenger.PassengerType();
+//Staff staff = new Staff {
+//    FirstName = "StaffFName",
+//    LastName = "StaffLN"
+//};
+//Traveller traveller = new Traveller {
+//    Nationality = "Tunisian",
+//    FirstName = "FNTraveller"
+//};
+//staff.PassengerType();
+//traveller.PassengerType();
+
+/*******Atelier 2*******/
+FlightMethods flightMethods = new FlightMethods
 {
-    Capacity = 300,
-    ManufactureDate = DateTime.Now,
-    PlaneId = 3,
-    PlaneType = PlaneType.Airbus
+    Flights = TestData.listFlights
 };
-Plane plane4 = new Plane { };
-Console.WriteLine(plane3.ToString());
-Console.WriteLine(plane4.ToString());
 
-Passenger passenger = new Passenger
+foreach (var item in flightMethods.GetFlightDates("Paris"))
 {
-    FirstName = "Seif",
-    LastName = "Znati",
-    EmailAddress = "seifeddine.znati@esprit.tn"
-};
-Console.WriteLine(passenger.ToString());
-Console.WriteLine(passenger.CheckProfile("Seif", "Znati"));
-Console.WriteLine(passenger.CheckProfile("Ons", "Znati"));
-Console.WriteLine(passenger.CheckProfile("Seif", "Znati","seifeddine.znati@esprit.tn"));
-Console.WriteLine(passenger.CheckProfile("Seif", "Znati", "znatiseif@gmail.com"));
-passenger.PassengerType();
-Staff staff = new Staff {
-    FirstName = "StaffFName",
-    LastName = "StaffLN"
-};
-Traveller traveller = new Traveller {
-    Nationality = "Tunisian",
-    FirstName = "FNTraveller"
-};
-staff.PassengerType();
-traveller.PassengerType();
+    Console.WriteLine(item);
+}
