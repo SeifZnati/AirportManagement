f6b7c87f0ffddf1f5566061541c071d2185d10b7
SeifZnati <seifeddine.znati@esprit.tn>
2025-02-20 09:54:45 +0100

    ORM

---
+++ AM.Infrastructure/AMContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AM.Infrastructure
+{
+    public class AMContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+                                        Initial Catalog=AirportManagementDB;
+                                        Integrated Security=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
